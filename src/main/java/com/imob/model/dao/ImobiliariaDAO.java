c32dd498bda5f81105c60b6f635df515ccd7690b
jhonz <jhonzincwb777@hotmail.com>
2023-06-27 17:58:05 -0300

    mudanças no dao e controller

--- src/main/java/com/imob/model/dao/ImobiliariaDAO.java
+++ src/main/java/com/imob/model/dao/ImobiliariaDAO.java
@@ -1,8 +1,6 @@
 package com.imob.model.dao;
 
 import com.imob.model.domain.Imobiliaria;
-
-import javax.swing.*;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -11,78 +9,95 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ImobiliariaDAO {
-    private Connection connection;
+
+    private final Connection connection;
 
     public ImobiliariaDAO(Connection connection) {
         this.connection = connection;
     }
 
-    public Connection getConnection() {
-        return connection;
+    public void inserirImobiliaria(Imobiliaria imobiliaria) throws SQLException {
+        String sql = "INSERT INTO tb_imobiliaria (id_Codigo_imobiliaria, numero_Creci, id_Imobiliaria, tb_pessoa_juridica_id_PessoaPJ, tb_pessoa_juridica_tb_pessoa_id_Pessoa, tb_pagamento_comissao_id_Pag_Comissao) VALUES (?, ?, ?, ?, ?, ?)";
+        PreparedStatement stmt = connection.prepareStatement(sql);
+        stmt.setInt(1, imobiliaria.getId_Codigo_imobiliaria());
+        stmt.setInt(2, imobiliaria.getNumero_Creci());
+        stmt.setInt(3, imobiliaria.getId_Imobiliaria());
+        stmt.setInt(4, imobiliaria.getTb_pessoa_juridica_id_PessoaPJ());
+        stmt.setInt(5, imobiliaria.getTb_pessoa_juridica_tb_pessoa_id_Pessoa());
+        stmt.setInt(6, imobiliaria.getTb_pagamento_comissao_id_Pag_Comissao());
+        stmt.executeUpdate();
+        stmt.close();
     }
 
-    public void setConnection(Connection connection) {
-        this.connection = connection;
+    public void atualizarImobiliaria(Imobiliaria imobiliaria) throws SQLException {
+        String sql = "UPDATE tb_imobiliaria SET id_Codigo_imobiliaria = ?, numero_Creci = ?, tb_pessoa_juridica_id_PessoaPJ = ?, tb_pessoa_juridica_tb_pessoa_id_Pessoa = ?, tb_pagamento_comissao_id_Pag_Comissao = ? WHERE id_Imobiliaria = ?";
+        PreparedStatement stmt = connection.prepareStatement(sql);
+        stmt.setInt(1, imobiliaria.getId_Codigo_imobiliaria());
+        stmt.setInt(2, imobiliaria.getNumero_Creci());
+        stmt.setInt(3, imobiliaria.getTb_pessoa_juridica_id_PessoaPJ());
+        stmt.setInt(4, imobiliaria.getTb_pessoa_juridica_tb_pessoa_id_Pessoa());
+        stmt.setInt(5, imobiliaria.getTb_pagamento_comissao_id_Pag_Comissao());
+        stmt.setInt(6, imobiliaria.getId_Imobiliaria());
+        stmt.executeUpdate();
+        stmt.close();
     }
 
-    public boolean inserirImobiliaria(Imobiliaria imobiliaria) throws SQLException {
-        String sql = "INSERT INTO tb_imobiliaria (id_Codigo_imobiliaria, numero_Creci, id_Imobiliaria) VALUES (?, ?, ?)";
-
-        try (PreparedStatement stmt = connection.prepareStatement(sql)) {
-            stmt.setInt(1, imobiliaria.getId_Codigo_imobiliaria());
-            stmt.setInt(2, imobiliaria.getNumero_Creci());
-            stmt.setInt(3, imobiliaria.getId_Imobiliaria());
-
-            stmt.execute();
-            return true;
-
-        } catch (SQLException ex) {
-            JOptionPane.showMessageDialog(null, "Não foi possível inserir no banco: " + ex);
-            return false;
-        }
-    }
-
-    public boolean atualizarImobiliaria(Imobiliaria imobiliaria) throws SQLException {
-        String sql = "UPDATE tb_imobiliaria SET id_Codigo_imobiliaria = ?, numero_Creci = ? WHERE id_Imobiliaria = ?";
-
-        try (PreparedStatement stmt = connection.prepareStatement(sql)) {
-            stmt.setInt(1, imobiliaria.getId_Codigo_imobiliaria());
-            stmt.setInt(2, imobiliaria.getNumero_Creci());
-            stmt.setInt(3, imobiliaria.getId_Imobiliaria());
-
-            stmt.executeUpdate();
-
-        }
-        return false;
-    }
-
-    public boolean excluirImobiliaria(Imobiliaria imobiliaria)throws SQLException {
+    public void excluirImobiliaria(Imobiliaria imobiliaria) throws SQLException {
         String sql = "DELETE FROM tb_imobiliaria WHERE id_Imobiliaria = ?";
-
-        try (PreparedStatement stmt = connection.prepareStatement(sql)){
-            stmt.setInt(1, imobiliaria.getId_Imobiliaria());
-
-            stmt.executeUpdate();
-        }
-        return false;
+        PreparedStatement stmt = connection.prepareStatement(sql);
+        stmt.setInt(1, imobiliaria.getId_Imobiliaria());
+        stmt.executeUpdate();
+        stmt.close();
     }
 
     public List<Imobiliaria> buscarTodasImobiliaria() throws SQLException {
-        List<Imobiliaria> imobiliarias = new ArrayList<>();
         String sql = "SELECT * FROM tb_imobiliaria";
+        PreparedStatement stmt = connection.prepareStatement(sql);
+        ResultSet rs = stmt.executeQuery();
 
-        try (PreparedStatement stmt = connection.prepareStatement(sql)) {
-            ResultSet resultado = stmt.executeQuery();
+        List<Imobiliaria> imobiliarias = new ArrayList<>();
+        while (rs.next()) {
+            int id_Codigo_imobiliaria = rs.getInt("id_Codigo_imobiliaria");
+            int numero_Creci = rs.getInt("numero_Creci");
+            int id_Imobiliaria = rs.getInt("id_Imobiliaria");
+            int tb_pessoa_juridica_id_PessoaPJ = rs.getInt("tb_pessoa_juridica_id_PessoaPJ");
+            int tb_pessoa_juridica_tb_pessoa_id_Pessoa = rs.getInt("tb_pessoa_juridica_tb_pessoa_id_Pessoa");
+            int tb_pagamento_comissao_id_Pag_Comissao = rs.getInt("tb_pagamento_comissao_id_Pag_Comissao");
 
-            while (resultado.next()) {
-                int id_Imobiliaria = resultado.getInt("id_Imobiliaria");
-                int id_Codigo_imobiliaria = resultado.getInt("id_Codigo_imobiliaria");
-                int numero_Creci = Integer.parseInt(resultado.getString("numero_Creci"));
-
-                Imobiliaria imobiliaria = new Imobiliaria(id_Imobiliaria, id_Codigo_imobiliaria, numero_Creci);
-                imobiliarias.add(imobiliaria);
-            }
+            Imobiliaria imobiliaria = new Imobiliaria(id_Codigo_imobiliaria, numero_Creci, id_Imobiliaria, tb_pessoa_juridica_id_PessoaPJ, tb_pessoa_juridica_tb_pessoa_id_Pessoa, tb_pagamento_comissao_id_Pag_Comissao);
+            imobiliarias.add(imobiliaria);
         }
+
+        rs.close();
+        stmt.close();
+
         return imobiliarias;
+    }
+
+    public List<Imobiliaria> lista() throws SQLException {
+        String sql = "SELECT * FROM tb_imobiliaria";
+        PreparedStatement stmt = connection.prepareStatement(sql);
+        ResultSet rs = stmt.executeQuery();
+
+        List<Imobiliaria> imobiliarias = new ArrayList<>();
+        while (rs.next()) {
+            int id_Codigo_imobiliaria = rs.getInt("id_Codigo_imobiliaria");
+            int numero_Creci = rs.getInt("numero_Creci");
+            int id_Imobiliaria = rs.getInt("id_Imobiliaria");
+            int tb_pessoa_juridica_id_PessoaPJ = rs.getInt("tb_pessoa_juridica_id_PessoaPJ");
+            int tb_pessoa_juridica_tb_pessoa_id_Pessoa = rs.getInt("tb_pessoa_juridica_tb_pessoa_id_Pessoa");
+            int tb_pagamento_comissao_id_Pag_Comissao = rs.getInt("tb_pagamento_comissao_id_Pag_Comissao");
+
+            Imobiliaria imobiliaria = new Imobiliaria(id_Codigo_imobiliaria, numero_Creci, id_Imobiliaria, tb_pessoa_juridica_id_PessoaPJ, tb_pessoa_juridica_tb_pessoa_id_Pessoa, tb_pagamento_comissao_id_Pag_Comissao);
+            imobiliarias.add(imobiliaria);
+        }
+
+        rs.close();
+        stmt.close();
+
+        return imobiliarias;
+    }
+
+    public void setConnection(Connection connection) {
     }
 }
--- src/main/java/com/imob/view/FXMLImobiliariaController.java
+++ src/main/java/com/imob/view/FXMLImobiliariaController.java
@@ -4,6 +4,8 @@ import com.imob.model.dao.ImobiliariaDAO;
 import com.imob.model.database.Database;
 import com.imob.model.database.DatabaseFactory;
 import com.imob.model.domain.Imobiliaria;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -12,6 +14,7 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 
@@ -36,6 +39,12 @@ public class FXMLImobiliariaController implements Initializable {
     @FXML
     private TableColumn<Imobiliaria, Integer> tbCodigoImobiliaria;
     @FXML
+    private TableColumn<Imobiliaria, Integer> tbPessoaPJ;
+    @FXML
+    private TableColumn<Imobiliaria, Integer> tbPessoaIdPessoa;
+    @FXML
+    private TableColumn<Imobiliaria, Integer> tbPagComissao;
+    @FXML
     private Button btInserir;
     @FXML
     private Button btPesquisar;
@@ -46,27 +55,47 @@ public class FXMLImobiliariaController implements Initializable {
     @FXML
     private Label lblCodigoCreci;
     @FXML
-    private TextField tfCodigoCreci;
+    private Label lblPessoaPj;
+    @FXML
+    private Label lblPagcomissao;
+    @FXML
+    private Label lblIdImobiliaria;
     @FXML
     private Label lblCodigoImobiliaria;
     @FXML
+    private Label lblPessoaId;
+    @FXML
+    private TextField tfCodigoCreci;
+    @FXML
     private TextField tfIdImobiliaria;
+    @FXML
+    private TextField tfPessoaPj;
+    @FXML
+    private TextField tfPessoaId;
+    @FXML
+    private TextField tfPagcomissao;
     @FXML
     private TextField tfCodigoImobiliaria;
 
-    public FXMLImobiliariaController(TableView<Imobiliaria> tableViewImobiliaria) {
-        this.tableViewImobiliaria = tableViewImobiliaria;
-    }
+    @FXML
+    private Label lblFecharImobiliaria;
+    private List<Imobiliaria> listImobiliaria;
+    private ObservableList<Imobiliaria> observableImobiliaria;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-
         imobiliariaDAO.setConnection(connection);
+        try {
+            carregaImobiliariaNaTableView();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println("Controlador inicializado");
     }
 
     @FXML
     private void fecharImobiliaria(MouseEvent event) {
-        Stage stage = (Stage) lblCodigoImobiliaria.getScene().getWindow();
+        Stage stage = (Stage) lblFecharImobiliaria.getScene().getWindow();
         stage.close();
     }
 
@@ -74,9 +103,12 @@ public class FXMLImobiliariaController implements Initializable {
     private void adicionarImobiliaria(ActionEvent event) {
         int id_Codigo_imobiliaria = Integer.parseInt(tfCodigoImobiliaria.getText());
         int numero_Creci = Integer.parseInt(tfCodigoCreci.getText());
-        int id_imobiliaria = Integer.parseInt(tfIdImobiliaria.getText());
+        int id_Imobiliaria = Integer.parseInt(tfIdImobiliaria.getText());
+        int tb_pessoa_juridica_id_PessoaPJ = Integer.parseInt(tfPessoaPj.getText());
+        int tb_pessoa_juridica_tb_pessoa_id_Pessoa = Integer.parseInt(tfPessoaId.getText());
+        int tb_pagamento_comissao_id_Pag_Comissao = Integer.parseInt(tfPagcomissao.getText());
 
-        Imobiliaria imobiliaria = new Imobiliaria(id_Codigo_imobiliaria, numero_Creci, id_imobiliaria);
+        Imobiliaria imobiliaria = new Imobiliaria(id_Codigo_imobiliaria, numero_Creci, id_Imobiliaria, tb_pessoa_juridica_id_PessoaPJ, tb_pessoa_juridica_tb_pessoa_id_Pessoa, tb_pagamento_comissao_id_Pag_Comissao);
 
         try {
             imobiliariaDAO.inserirImobiliaria(imobiliaria);
@@ -89,9 +121,12 @@ public class FXMLImobiliariaController implements Initializable {
     private void atualizarImobiliaria(ActionEvent event) {
         int id_Codigo_imobiliaria = Integer.parseInt(tfCodigoImobiliaria.getText());
         int numero_Creci = Integer.parseInt(tfCodigoCreci.getText());
-        int id_imobiliaria = Integer.parseInt(tfIdImobiliaria.getText());
+        int id_Imobiliaria = Integer.parseInt(tfIdImobiliaria.getText());
+        int tb_pessoa_juridica_id_PessoaPJ = Integer.parseInt(tfPessoaPj.getText());
+        int tb_pessoa_juridica_tb_pessoa_id_Pessoa = Integer.parseInt(tfPessoaId.getText());
+        int tb_pagamento_comissao_id_Pag_Comissao = Integer.parseInt(tfPagcomissao.getText());
 
-        Imobiliaria imobiliaria = new Imobiliaria(id_Codigo_imobiliaria, numero_Creci, id_imobiliaria);
+        Imobiliaria imobiliaria = new Imobiliaria(id_Codigo_imobiliaria, numero_Creci, id_Imobiliaria, tb_pessoa_juridica_id_PessoaPJ, tb_pessoa_juridica_tb_pessoa_id_Pessoa, tb_pagamento_comissao_id_Pag_Comissao);
 
         try {
             imobiliariaDAO.atualizarImobiliaria(imobiliaria);
@@ -103,10 +138,10 @@ public class FXMLImobiliariaController implements Initializable {
 
     @FXML
     private void excluirImobiliaria(ActionEvent event) {
-        int id_imobiliaria = Integer.parseInt(tfIdImobiliaria.getText());
+        int id_Imobiliaria = Integer.parseInt(tfIdImobiliaria.getText());
 
         try {
-            Imobiliaria imobiliaria = new Imobiliaria(id_imobiliaria, 0, 0);
+            Imobiliaria imobiliaria = new Imobiliaria(0, 0, id_Imobiliaria, 0, 0, 0);
             imobiliariaDAO.excluirImobiliaria(imobiliaria);
             System.out.println("Imobiliária excluída com sucesso!");
         } catch (SQLException e) {
@@ -121,6 +156,32 @@ public class FXMLImobiliariaController implements Initializable {
         } catch (SQLException e) {
             System.out.println("Erro ao buscar imobiliárias: " + e.getMessage());
             return null;
+        }
+    }
+
+    public void carregaImobiliariaNaTableView() throws SQLException {
+        tbCreci.setCellValueFactory(new PropertyValueFactory<>("numero_Creci"));
+        tbIdImobiliaria.setCellValueFactory(new PropertyValueFactory<>("id_Imobiliaria"));
+        tbCodigoImobiliaria.setCellValueFactory(new PropertyValueFactory<>("id_Codigo_imobiliaria"));
+        tbPessoaPJ.setCellValueFactory(new PropertyValueFactory<>("tb_pessoa_juridica_id_PessoaPJ"));
+        tbPessoaIdPessoa.setCellValueFactory(new PropertyValueFactory<>("tb_pessoa_juridica_tb_pessoa_id_Pessoa"));
+        tbPagComissao.setCellValueFactory(new PropertyValueFactory<>("tb_pagamento_comissao_id_Pag_Comissao"));
+
+        listImobiliaria = imobiliariaDAO.lista();
+        observableImobiliaria = FXCollections.observableArrayList(listImobiliaria);
+        tableViewImobiliaria.setItems(observableImobiliaria);
+    }
+
+    @FXML
+    private void selecionarLinhaViewTable(MouseEvent event) {
+        Imobiliaria imobiliaria = tableViewImobiliaria.getSelectionModel().getSelectedItem();
+        if (imobiliaria != null) {
+            tfCodigoCreci.setText(String.valueOf(imobiliaria.getNumero_Creci()));
+            tfIdImobiliaria.setText(String.valueOf(imobiliaria.getId_Imobiliaria()));
+            tfCodigoImobiliaria.setText(String.valueOf(imobiliaria.getId_Codigo_imobiliaria()));
+            tfPessoaPj.setText(String.valueOf(imobiliaria.getTb_pessoa_juridica_id_PessoaPJ()));
+            tfPessoaId.setText(String.valueOf(imobiliaria.getTb_pessoa_juridica_tb_pessoa_id_Pessoa()));
+            tfPagcomissao.setText(String.valueOf(imobiliaria.getTb_pagamento_comissao_id_Pag_Comissao()));
         }
     }
 }
